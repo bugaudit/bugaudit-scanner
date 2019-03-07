d4c8ea94973b936e1df7bb2a221a7cb3cfb049e3
Shibly Meeran <git@shib.me>
2019-03-07 12:10:47 +0530

    updating to provide probe dir

--- src/main/java/me/shib/bugaudit/probe/ProbeConfig.java
+++ src/main/java/me/shib/bugaudit/probe/ProbeConfig.java
@@ -1,14 +1,31 @@
 package me.shib.bugaudit.probe;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class ProbeConfig {
 
+    private static final String probeDirPathEnv = "BUGAUDIT_PROBE_DIR";
+
     private Map<String, Integer> priorityMap;
+    private transient File probeDir;
 
     public ProbeConfig() {
         this.priorityMap = new HashMap<>();
+        probeDir = makeProbeDir();
+
+    }
+
+    private File makeProbeDir() {
+        String probeDirPath = System.getenv(probeDirPathEnv);
+        if (probeDirPath != null && !probeDirPath.isEmpty()) {
+            File dir = new File(probeDirPath);
+            if (dir.exists() && dir.isDirectory()) {
+                return dir;
+            }
+        }
+        return new File(System.getenv("user.dir"));
     }
 
     protected abstract Map<String, Integer> getDefaultPriorityMap();
@@ -21,6 +38,10 @@ public abstract class ProbeConfig {
             }
         }
         return priorityMap;
+    }
+
+    public File getProbeDir() {
+        return probeDir;
     }
 
     public void addPriorityForType(String type, int priority) {
